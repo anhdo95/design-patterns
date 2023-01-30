0ee95f80c9cbd4d22f5f2514ca6ca1b893ccd89d
Anh Do <do.dinh.anh@linecorp.com>
2023-01-30 10:48:46 +0700

    Simply use a regular object to achieve the Singleton pattern result

--- singleton/cache.js
+++ singleton/cache.js
@@ -1,36 +1,24 @@
-class Cache {
-  constructor() {
-    if (Cache.instance) {
-      throw new Error('You can only create one instance!')
-    }
+const cache = new Map()
 
-    this.cache = new Map()
-    Cache.instance = this
-  }
-
+// `Object.freeze`, its methods cannot be changed, nor can new methods or properties be added
+export default Object.freeze({
   set(key, value) {
-    this.cache.set(key, value)
-  }
+    cache.set(key, value)
+  },
 
   get(key) {
-    return this.cache.get(key)
-  }
+    return cache.get(key)
+  },
 
   has(key) {
-    return this.cache.has(key)
-  }
+    return cache.has(key)
+  },
 
   delete(key) {
-    if (!this.has(key)) {
+    if (!has(key)) {
       return false
     }
 
-    return this.cache.delete(key)
-  }
-}
-
-// This ensures that the newly created instance is not modifiable.
-const cache = Object.freeze(new Cache())
-
-// Exporting the variable as the `default` value within the file to make it globally accessible.
-export default cache
+    return cache.delete(key)
+  },
+})
